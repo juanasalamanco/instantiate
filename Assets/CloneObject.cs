eb316f9a1196ea79c9ac8c0a5f2af811175b04ee
juanasalamanco <juanasalamanco@gmail.com>
2022-05-04 15:49:38 -0300

    Initial commit

---
+++ Assets/CloneObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObject : MonoBehaviour
+{
+    public GameObject objectToClone;
+    
+    public void InstantiateObject()
+    {
+        int i = 0;
+        while (i < 5)
+        {
+            Instantiate(objectToClone);
+            i++;
+        }        
+    }
+}
