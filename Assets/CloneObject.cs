e1aaa5a70b3440e1ba130539af57aba512292766
juanasalamanco <juanasalamanco@gmail.com>
2022-05-10 08:40:28 -0300

    update

--- Assets/CloneObject.cs
+++ Assets/CloneObject.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class CloneObject : MonoBehaviour
 {
     public GameObject objectToClone;
-    
+    //public GameObject clon;
+
     public void InstantiateObject()
     {
+        /*
         int i = 0;
         while (i < 5)
         {
             Instantiate(objectToClone);
             i++;
-        }        
+        }
+        */    
+
+        for(int i = 0; i < 13; i++)
+        {
+            GameObject clon;
+            clon = Instantiate(objectToClone);
+            Destroy(clon, 1);
+        }
+
     }
 }
